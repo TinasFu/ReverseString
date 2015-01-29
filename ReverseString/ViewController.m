e61f7aba19b953d21afefe03d3b2874a06cab67c
Tina Fu <TinasFu@outlook.com>
2015-01-28 23:44:00 -0800

    ReverseString App

--- ReverseString/ViewController.m
+++ ReverseString/ViewController.m
@@ -10,18 +10,67 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+@property (retain, nonatomic) IBOutlet UILabel *textLabel;
+
+-(NSString *)reverseStr:(NSString*)str;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.textField.delegate = self;
+    UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self
+                                                                          action:@selector(dismissKeyboard)];
+    
+    [self.view addGestureRecognizer:tap];
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)dismissKeyboard {
+    [self.view endEditing:YES];
 }
+
+
+
+-(void)textFieldDidEndEditing:(UITextField *)textField{
+    self.textLabel.text = self.textField.text;
+    NSLog(@"%@", self.textField.text);
+}
+
+
+- (IBAction)reverseStringPressed:(id)sender {
+
+    [self.view endEditing:YES];
+    NSString *errorMessage = nil;
+    if (self.textField.text.length == 0) {
+        errorMessage = @"You must provide a string to reverse.";
+        self.textLabel.text = errorMessage;
+    }
+    
+    else {
+        NSString *inputString = self.textField.text;
+        
+        self.textLabel.text = [self reverseStr:inputString];
+    }
+    
+}
+
+-(NSString *)reverseStr:(NSString*)str {
+    int length = [str length];
+    NSMutableString *reversedString;
+    
+    reversedString = [[NSMutableString alloc] initWithCapacity: length];
+    
+    while (length > 0) {
+        [reversedString appendString:[NSString stringWithFormat:@"%C", [str characterAtIndex:--length]]];
+    }
+    
+    return reversedString;
+}
+
+
 
 @end
